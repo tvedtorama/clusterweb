211d68695d82324c1d556f1040fad8470f4232af
Jørgen Tvedt <tvedtorama@gmail.com>
2018-07-09 14:21:17 +0200

    React-router test.  (big mistake with upper case Component... fixed)

--- app/components/WebMain.tsx
+++ app/components/WebMain.tsx
@@ -1,8 +1,32 @@
 import * as React from 'react'
+import { Link, BrowserRouter as Router, Route, match } from 'react-router-dom';
+
+const HeiComp: React.StatelessComponent<{match: match<any>}> = ({match}) =>
+	<span>We got hit {match.isExact ? "exactly" : "approximately"}</span>
+
+// class HeiComp
+
+const HalloComp = (props) => <span>You got hallo</span>
 
 class WebMainRaw extends React.Component<{store: any}> {
 	render() {
-		return <div><span>This is t</span></div>
+		return <div>
+			
+			<Router>
+				<div>
+					<p style={{display: "flex", flexDirection: "column" }}>
+						<span>This is the <Link to="/hei">Hei Link</Link></span>
+						<span>This is the <Link to="/hei/paaDeg">HeiPaaDeg Link</Link></span>
+					</p>
+					<p style={{display: "flex", flexDirection: "column" }}>
+						<span>The Route outputs:</span>
+						<Route exact path="/" component={(p) => <span>Rootish!</span>} />
+						<Route path="/hei" component={HeiComp} />
+						<Route path="/hallo" component={HalloComp} />
+					</p>
+				</div>
+			</Router>
+		</div>
 	}
 }
 
