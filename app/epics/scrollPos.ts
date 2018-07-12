a503d56800894031ed3e60e1260e8cd930c846c0
Jørgen Tvedt <tvedtorama@gmail.com>
2018-07-12 09:22:16 +0200

    Fixed loop so latest event is picked, not first.

--- app/epics/scrollPos.ts
+++ app/epics/scrollPos.ts
@@ -1,11 +1,11 @@
 import { Observable, fromEvent, interval } from "rxjs";
-import { map, filter, buffer, takeLast, repeat } from "rxjs/operators";
+import { map, filter, buffer } from "rxjs/operators";
 import { Action } from "redux";
 import { SET_EVENT_DATA } from "../../storyAnim/actions/eventData";
 import { animationFrame } from "rxjs/internal/scheduler/animationFrame";
-import { last } from "../../node_modules/ix/iterable";
 
 // TODO: Bring in the start and end position in the scroll percentage.  Should stay at 0% or 100% when the sliding box is not moving.
+// Note: Would really like to use something other than buffer, to preserve the observable nature.  `window` seems to be an option, but I think it will delay the input by a frame.
 export const scrollPosEpic = (action$: Observable<Action>) =>
 	fromEvent(document, "scroll").pipe(
 		map((e: UIEvent) => (e.target as Document).scrollingElement),
@@ -13,6 +13,6 @@ export const scrollPosEpic = (action$: Observable<Action>) =>
 		map(e => ({pos: e.scrollTop * 100 / (e.scrollHeight - e.clientHeight)})),
 		map(x => ({type: SET_EVENT_DATA, eventData: <StoryAnim.IEventData>{...x, type: "SCROLL_POS"}})),
 		buffer(interval(0, animationFrame)),
-		map(x => x[0]),
-		filter(x => x ? true : false),
+		filter(x => x.length > 0),
+		map(x => x[x.length - 1]),
 	)
