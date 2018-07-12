e701063854aa22280c09ec47e7e506a8459d9a19
Jørgen Tvedt <tvedtorama@gmail.com>
2018-07-12 09:09:49 +0200

    Introduced animationFrame scheduling.

--- app/epics/scrollPos.ts
+++ app/epics/scrollPos.ts
@@ -1,7 +1,9 @@
-import { Observable, fromEvent } from "rxjs";
-import { map, filter } from "rxjs/operators";
+import { Observable, fromEvent, interval } from "rxjs";
+import { map, filter, buffer, takeLast, repeat } from "rxjs/operators";
 import { Action } from "redux";
 import { SET_EVENT_DATA } from "../../storyAnim/actions/eventData";
+import { animationFrame } from "rxjs/internal/scheduler/animationFrame";
+import { last } from "../../node_modules/ix/iterable";
 
 // TODO: Bring in the start and end position in the scroll percentage.  Should stay at 0% or 100% when the sliding box is not moving.
 export const scrollPosEpic = (action$: Observable<Action>) =>
@@ -9,5 +11,8 @@ export const scrollPosEpic = (action$: Observable<Action>) =>
 		map((e: UIEvent) => (e.target as Document).scrollingElement),
 		filter(e => e.tagName === "HTML"),
 		map(e => ({pos: e.scrollTop * 100 / (e.scrollHeight - e.clientHeight)})),
-		map(x => ({type: SET_EVENT_DATA, eventData: <StoryAnim.IEventData>{...x, type: "SCROLL_POS"}}))
+		map(x => ({type: SET_EVENT_DATA, eventData: <StoryAnim.IEventData>{...x, type: "SCROLL_POS"}})),
+		buffer(interval(0, animationFrame)),
+		map(x => x[0]),
+		filter(x => x ? true : false),
 	)
