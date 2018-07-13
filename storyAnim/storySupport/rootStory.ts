4c562b0b4b492699c6ffc0674605432cb0ba1188
Jørgen Tvedt <tvedtorama@gmail.com>
2018-07-13 13:08:03 +0200

    Switched to use action method instead of action code directly.

--- storyAnim/storySupport/rootStory.ts
+++ storyAnim/storySupport/rootStory.ts
@@ -1,5 +1,5 @@
 import { IStoryRunnerProvider } from "../storyRunner";
-import { STORE_STORY_ITEM } from "../actions/storyItem";
+import { STORE_STORY_ITEM, storeStoryItem } from "../actions/storyItem";
 import { NOP } from "../actions/nop";
 import { filterChildren } from "./filterChildren";
 
@@ -10,17 +10,14 @@ export const ROOT_STORY_COMPONENT = ROOT_STORY_ID
 export const getRootStory = (storySetup: IStoryRunnerProvider): IStoryRunnerProvider => ({
 	id: ROOT_STORY_ID,
 	getStory: function*() {
-		yield {
-			type: STORE_STORY_ITEM,
-			payload: <StoryAnimDataSchema.IStoryItem>{
+		yield storeStoryItem({
 				position: {}, // ATW: Position not used for real root
 				id: ROOT_STORY_ID,
 				visual: {
 					component: ROOT_STORY_COMPONENT,
 					props: {}
 				}
-			}
-		}
+			})
 		// Root must not exit, it will stop the animation
 		while (true) {
 			yield {type: NOP}
