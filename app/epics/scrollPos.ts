7ac99d13d4cda7a0b85d6bae9462c399b0334fe2
Jørgen Tvedt <tvedtorama@gmail.com>
2018-07-25 09:50:24 +0200

    Safari fix - seems to use different element for scrolling, and clientHeight.

--- app/epics/scrollPos.ts
+++ app/epics/scrollPos.ts
@@ -4,13 +4,16 @@ import { Action } from "redux";
 import { SET_EVENT_DATA } from "../../storyAnim/actions/eventData";
 import { animationFrame } from "rxjs/internal/scheduler/animationFrame";
 
+const getClientHeight = (e: Element) =>
+	e.tagName === "BODY" ? e.parentElement.clientHeight : e.clientHeight
+
 // TODO: Bring in the start and end position in the scroll percentage.  Should stay at 0% or 100% when the sliding box is not moving.
 // Note: Would really like to use something other than buffer, to preserve the observable nature.  `window` seems to be an option, but I think it will delay the input by a frame.
 export const scrollPosEpic = (action$: Observable<Action>) =>
 	fromEvent(document, "scroll").pipe(
 		map((e: UIEvent) => (e.target as Document).scrollingElement),
-		filter(e => e.tagName === "HTML"),
-		map(e => ({pos: e.scrollTop * 100 / (e.scrollHeight - e.clientHeight)})),
+		filter(e => e.tagName === "HTML" || e.tagName === "BODY"),
+		map(e => ({pos: e.scrollTop * 100 / (e.scrollHeight - getClientHeight(e))})),
 		map(x => ({type: SET_EVENT_DATA, eventData: <StoryAnim.IEventData>{...x, type: "SCROLL_POS"}})),
 		buffer(interval(0, animationFrame)),
 		filter(x => x.length > 0),
