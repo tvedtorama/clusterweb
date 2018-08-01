fcfcb06ecf4d3eb6193859ea48a747d1bd8c4bae
Jørgen Tvedt <tvedtorama@gmail.com>
2018-08-01 13:23:54 +0200

    Some text and missing keys.

--- app/components/slides/Detorit.tsx
+++ app/components/slides/Detorit.tsx
@@ -3,11 +3,11 @@ import * as React from 'react'
 export class DetroitIntro extends React.Component {
 	render() {
 		return [
-			<h1>Detorit, USA</h1>,
-			<p>The US in the 20'ies had a good set of car companies...</p>,
-			<ul>
+			<h1 key="h1">Detorit, USA</h1>,
+			<p key="p">>The US in the 20'ies had a good set of car companies...</p>,
+			<ul key="ul">>
 				<li>Huge demand</li>
-				<li>abc 123</li>
+				<li>All companies and their mergers ended up in Detroit</li>
 				<li>123 dsflkj</li>
 			</ul>
 		]
