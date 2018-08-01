661b9a800838d388ba2e7c8f28018011567d8f56
Jørgen Tvedt <tvedtorama@gmail.com>
2018-08-01 14:17:47 +0200

    Fixed typos

--- app/components/slides/Detorit.tsx
+++ app/components/slides/Detorit.tsx
@@ -4,8 +4,8 @@ export class DetroitIntro extends React.Component {
 	render() {
 		return [
 			<h1 key="h1">Detorit, USA</h1>,
-			<p key="p">>The US in the 20'ies had a good set of car companies...</p>,
-			<ul key="ul">>
+			<p key="p">The US in the 20'ies had a good set of car companies...</p>,
+			<ul key="ul">
 				<li>Huge demand</li>
 				<li>All companies and their mergers ended up in Detroit</li>
 				<li>123 dsflkj</li>
