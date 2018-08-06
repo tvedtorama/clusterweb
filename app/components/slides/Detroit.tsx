166d5247ab2e5a8a25b9d55314e9f0bcab74eacd
Jørgen Tvedt <tvedtorama@gmail.com>
2018-08-06 13:34:45 +0200

    Fixed typo

--- app/components/slides/Detroit.tsx
+++ app/components/slides/Detroit.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 export class DetroitIntro extends React.Component {
 	render() {
 		return [
-			<h1 key="h1">Detorit, USA</h1>,
+			<h1 key="h1">Detroit, USA</h1>,
 			<p key="p">The US in the 20'ies had a good set of car companies...</p>,
 			<ul key="ul">
 				<li>Huge demand</li>
