79ba27a023dd5715005043e4f0a9c298b2fab466
Jørgen Tvedt <tvedtorama@gmail.com>
2018-08-07 18:07:44 +0200

    Content

--- app/components/slides/DetroitDetail.tsx
+++ app/components/slides/DetroitDetail.tsx
@@ -4,7 +4,7 @@ export class DetroitDetail extends React.Component {
 	render() {
 		return [
 			<h1 key="h1">Detroit - Motor City</h1>,
-			<p key="p">From then on, the area was the place to be for the US and Canadian motor industry.  This was simply where all the business was taking place, where the right people lived and where personal networks was built.</p>,
+			<p key="p">From then on, the area was the place to be for the US and Canadian motor industry.  This was simply where all the business was taking place, where the right people could be found and where personal networks was built.</p>,
 		]
 	}
 }
