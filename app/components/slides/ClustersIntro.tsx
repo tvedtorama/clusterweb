172a785a6eab12dc53633e4e8242fc9ef1f4ae97
Jørgen Tvedt <tvedtorama@gmail.com>
2018-08-08 13:41:29 +0200

    Text changes.

--- app/components/slides/ClustersIntro.tsx
+++ app/components/slides/ClustersIntro.tsx
@@ -4,7 +4,7 @@ export class ClustersIntro extends React.Component {
 	render() {
 		return [
 			<h1 key="h">Industrial Clusters</h1>,
-			<p key="p">Clusters are groups of organizations from different industries, that together deliver exceptional performance.  They typically consists of some big companies in front with competing sub-contractors.  Dig deeper and you'll find a flow of people and skills accross the company borders.<br/><br/><code>Traditionally</code> they have been known to rely on geographical closeness and personal networks.</p>,
+			<p key="p">Clusters are groups of organizations from different industries, that together deliver exceptional performance.  They typically consists of some big companies in front with competing sub-contractors.  Dig deeper and you'll find a flow of people and skills crossing the company borders.<br/><br/><code>Traditionally</code> they have relied on geographical proximity, personal networks and hard earned trust.</p>,
 			<span key="s">Lets look at some examples...</span>
 		]
 	}
