2b1d8a9d443abb207c39787f7c365639caf36979
Jørgen Tvedt <tvedtorama@gmail.com>
2018-08-10 07:27:07 +0200

    Adjusted size of rig image to work on mobile, no idea what happens here.

--- app/components/images/index.ts
+++ app/components/images/index.ts
@@ -6,7 +6,7 @@ import OilRig from '../../img/oilrig.svg'
 // Note: Could introduce a generic wrapping here, to allow fade out and different kinds of motion.
 export const imageComponentMap = {
 	HTML_BOAT: props => React.createElement(Boat, {width: 600, height: 450, viewBox: "300 0 700 450", className: "svg-image"}),
-	HTML_OIL_RIG: props => React.createElement(OilRig, {width: 800, height: 650, viewBox: "300 0 700 1050", className: "svg-image"}),
+	HTML_OIL_RIG: props => React.createElement(OilRig, {width: 800, height: 450, viewBox: "300 0 700 1050", className: "svg-image"}),
 }
 
 export type IImageKey = keyof typeof imageComponentMap
