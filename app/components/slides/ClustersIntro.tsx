884abb2898abbe0e5249859d19a17232841843b4
Jørgen Tvedt <tvedtorama@gmail.com>
2018-08-10 10:48:00 +0200

    Some thoughts

--- app/components/slides/ClustersIntro.tsx
+++ app/components/slides/ClustersIntro.tsx
@@ -2,6 +2,9 @@ import * as React from 'react'
 
 export class ClustersIntro extends React.Component {
 	render() {
+		// Issues:
+		// All companies are part of a cluster, not all are part of super-clusters
+		// All value networks are vital in the value creation - not all provide exceptional performance.
 		return [
 			<h1 key="h">Industrial Clusters</h1>,
 			<p key="p">Clusters are groups of organizations from different industries, that together deliver exceptional performance.  They typically consists of some big companies in front with competing sub-contractors.  Dig deeper and you'll find a flow of people and skills crossing the company borders.<br/><br/><code>Traditionally</code> they have relied on geographical proximity, personal networks and hard earned trust.</p>,
