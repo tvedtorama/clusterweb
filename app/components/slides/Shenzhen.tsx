87511a6de656336ec1e61fa3a41df0fea79050ac
Jørgen Tvedt <tvedtorama@gmail.com>
2018-08-10 07:40:19 +0200

    Text changes.

--- app/components/slides/Shenzhen.tsx
+++ app/components/slides/Shenzhen.tsx
@@ -6,9 +6,10 @@ export class ShenzhenIntro extends React.Component {
 			<h1 key="h">China, Shenzhen</h1>,
 			<p key="p">The rules was relaxed and creativity let loose in what would become the Silicon Valley of hardware</p>,
 			<ul key="ul">
-				<li>Sharing of IP</li>
-				<li>Great competition and highly skilled workforce</li>
-				<li>Lots of copying crap</li>
+				<li>Domestic and international customers shopping for suppliers</li>
+				<li>Dubious IP enforcment and copying leads to competition and innovation</li>
+				<li>Skilled workforce, competitive salaries</li>
+				<li>Access to world marked through shipping and air</li>
 			</ul>
 		]
 	}
