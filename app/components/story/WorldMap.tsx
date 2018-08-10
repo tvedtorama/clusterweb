337f229d2064b942305a23f5d54166028b00da34
Jørgen Tvedt <tvedtorama@gmail.com>
2018-08-10 07:27:29 +0200

    Added zurich, banking and watches.

--- app/components/story/WorldMap.tsx
+++ app/components/story/WorldMap.tsx
@@ -22,7 +22,8 @@ const cityCooridnates: [number, number][] = [
 	[-83.045754, 42.331427], // Detroit
 	[5.628929, 62.3317447], // Fosnavaag
 	[114.1, 22.55], // Shenzhen
-	[-0.893753, 52.230375] // Northampton
+	[-0.893753, 52.230375], // Northampton
+	[8.55, 47.366667], // Zürich
 ]
 
 const WorldMapContent: React.StatelessComponent<{projection, currentCity, worldData, scale}> = ({projection, currentCity, worldData, scale}) => [
