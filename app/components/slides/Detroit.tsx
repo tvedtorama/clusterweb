729fa6441436af7a2c6fd4f660a0a250ff609c38
Jørgen Tvedt <tvedtorama@gmail.com>
2018-08-15 12:55:06 +0200

    Text changes

--- app/components/slides/Detroit.tsx
+++ app/components/slides/Detroit.tsx
@@ -6,7 +6,7 @@ export class DetroitIntro extends React.Component {
 			<h1 key="h1">Detroit - Motor City</h1>,
 			<p key="p">By the 20ies, people had realized the value and freedom offered by automobiles, and the market was booming. Although initially challenged by contenders from around the country, all the major motor companies settled in the Detroit area.</p>,
 			<ul key="ul">
-				<li>Ford Motors with support from existing transport companies started the show</li>
+				<li>Ford Motors, with support from existing transport companies, started the show</li>
 				<li>Detroit had the tooling and sub-contractors needed by the industry</li>
 				<li>Major investments was initiated by leaders who knew the industry inside out</li>
 			</ul>
--- app/components/slides/NorwayOil.tsx
+++ app/components/slides/NorwayOil.tsx
@@ -4,9 +4,9 @@ export class NorwayOil extends React.Component {
 	render() {
 		return [
 			<h1 key="h">More Luck: Oil</h1>,
-			<p key="p">Below the fish, oil was discovered and developed from the 70ies.  The strong maritime cluster in the coastal villages started build support vessels for the oil industry.  Soon, they were dominating this area too.</p>,
+			<p key="p">Below the fish, oil was discovered and developed from the 70ies.  The strong maritime cluster in the coastal villages started building support vessels for the offshore petroleum activity.  Soon, they were dominating this industry too.</p>,
 			<ul key="u">
-				<li>The yards had the knowledge to build larger ships</li>
+				<li>The yards had the capacity to build larger ships</li>
 				<li>The shipping companies had the knowledge and capital to enter a new marked</li>
 				<li>Together, they offered world class shipping services for the emerging oil industry.</li>
 			</ul>
