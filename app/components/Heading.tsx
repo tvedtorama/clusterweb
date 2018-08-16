08de4b37bb2192a4160c7da528bccb16cec48019
Jørgen Tvedt <tvedtorama@gmail.com>
2018-08-16 10:43:22 +0200

    Added typed.js for animated text on heading, layout.

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -1,6 +1,23 @@
 import * as React from 'react'
+import {default as Typed, TypedOptions} from 'typed.js'
 
 class HeadingRaw extends React.Component {
+	typed: Typed
+	subtitle: HTMLSpanElement
+	componentDidMount() {
+		const options: TypedOptions = {
+			strings: [
+				"The Industrial Cluster Accelerator",
+				"See why clusters are amazing!",
+				"Scroll down to learn about clusters in the digital world.",
+			],
+			typeSpeed: 50,
+			backSpeed: 90,
+			loop: true,
+			startDelay: 1000 as any,
+		}
+		this.typed = new Typed(this.subtitle as any, options)
+	}
 	render() {
 		return <div className="heading">
 				<div className="background" />
@@ -10,7 +27,7 @@ class HeadingRaw extends React.Component {
 						<span className="produc-name">Phil Network</span>
 					</div>
 					<div className="subtitle-row row">
-						<span className="subtitle">The Industrial Cluster Accelerator</span>
+						<span className="subtitle" ref={r => this.subtitle = r}/>
 					</div>
 				</div>
 				<div className="scroll-text">
