eb2c71734056308201628fdbb99d949b742a6a20
Jørgen Tvedt <tvedtorama@gmail.com>
2018-08-17 18:55:18 +0200

    Typo

--- app/components/slides/Norway.tsx
+++ app/components/slides/Norway.tsx
@@ -4,7 +4,7 @@ export class NorwayIntro extends React.Component {
 	render() {
 		return [
 			<h1 key="h">Norway, Northern West Coast</h1>,
-			<p key="p">The oceans surrounding Norway is packed with swimming proteins. The locals has utilized this stroke of luck since before the vikings. Not only for local consumption, but also exports - in the form of dried fish.  Today, traditional fishing has been accompanied and surpassed by fish farming.</p>,
+			<p key="p">The oceans surrounding Norway is packed with swimming proteins. The locals have utilized this stroke of luck since before the vikings. Not only for local consumption, but also exports - in the form of dried fish.  Today, traditional fishing has been accompanied and surpassed by fish farming.</p>,
 			<ul key="ul">
 				<li>In the early 50ies, after the war, fishing vessels were in strong demand</li>
 				<li>The productive people of <b>small villages Ulsteinvik and Fosnavåg</b> tok the job</li>
